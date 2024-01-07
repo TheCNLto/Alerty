41a7627c6907514bf1ef71768b45fd844592e5d1
Q3U3 <39864872+TheCNLto@users.noreply.github.com>
2024-01-07 15:38:29 +0100

    V0.1

---
+++ main.py
@@ -0,0 +1,125 @@
+import time
+from customtkinter import CTk, CTkTabview, CTkLabel, CTkTextbox, CTkButton, CTkToplevel
+from tkinter import BOTH, X, messagebox
+from datetime import datetime, timedelta
+
+
+class alerty_alert(CTkToplevel):
+    def __init__(self, title, about, timed):
+        super().__init__()
+        self.title(title)
+        self.lb1 = CTkLabel(
+            master=self,
+            text=about
+        )
+        self.lb1.pack(fill=BOTH, expand=1, padx=20, pady=20)
+        self.lb2 = CTkLabel(
+            master=self,
+            text=timed
+        )
+        self.lb2.pack(fill=BOTH, expand=1, padx=20)
+        self.btn1 = CTkButton(
+            master=self,
+            text='Clear alert',
+            command=self.clearAlert
+        )
+
+    def clearAlert(self):
+        self.destroy()
+
+
+class main(CTk):
+    def __init__(self):
+        super().__init__()
+        self.alerts: list = []
+        self.title("Alerty")
+        self.tabs = CTkTabview(master=self)
+        self.create_tab = self.tabs.add("Create")
+        # self.exists_tab = self.tabs.add("Exists")
+        self.on_going_tab = self.tabs.add("On going")
+        self.tabs.pack(fill=BOTH, expand=1)
+        self.lb_create_1 = CTkLabel(
+            master=self.create_tab,
+            text="Title of the alert:"
+        )
+        self.lb_create_1.pack(fill=X, expand=0)
+        self.alert_title = CTkTextbox(
+            master=self.create_tab,
+            height=10
+        )
+        self.alert_title.pack(fill=X, expand=0)
+        self.lb_create_about = CTkLabel(
+            master=self.create_tab,
+            text="Description of the alert:"
+        )
+        self.lb_create_about.pack(fill=X, expand=0)
+        self.alert_about = CTkTextbox(
+            master=self.create_tab,
+            height=10
+        )
+        self.alert_about.pack(fill=X, expand=0)
+        self.lb_create_time = CTkLabel(
+            master=self.create_tab,
+            text="Time of the alert:"
+        )
+        self.lb_create_time.pack(fill=X, expand=0)
+        self.alert_time = CTkTextbox(
+            master=self.create_tab,
+            height=10
+        )
+        self.alert_time.pack(fill=X, expand=0)
+        self.alert_create_button = CTkButton(
+            text="Create timed alert",
+            master=self.create_tab,
+            command=self.createTimedAlert
+        )
+        self.alert_create_button.pack(fill=X, expand=0, pady=10)
+        self.eval('tk::PlaceWindow . center')
+        self.checker()
+
+    def checker(self):
+        current_time = time.localtime()
+        current_datetime = datetime.fromtimestamp(time.mktime(current_time))
+        new_datetime = current_datetime + timedelta(minutes=1)
+        new_time = time.localtime(time.mktime(new_datetime.timetuple()))
+        for i in self.alerts:
+            if i <= time.strftime("%H:%M", new_time):
+                alerty_alert(self.alert_title.get('0.0', 'end'), self.alert_about.get('0.0', 'end'), i)
+                print("time!!!!!")
+                self.alerts.remove(i)
+        self.after(200, self.checker)
+
+    def createTimedAlert(self):
+        # print(len(self.alert_title.get("0.0", "end")))
+        try:
+            storeTime = self.alert_time.get("0.0", "end")
+            self.alerts.append(storeTime)
+            lbCreated = CTkLabel(
+                master=self.on_going_tab,
+                text=f"{self.alert_title.get('0.0', 'end')}\n{self.alert_about.get('0.0', 'end')}\n{storeTime}"
+            )
+            lbCreated.pack(fill=X, expand=0, pady=20)
+
+            def x1():
+                try:
+                    self.alerts.remove(storeTime)
+                except Exception:
+                    pass
+                finally:
+                    lbCreated.destroy()
+                    clearBtn.destroy()
+
+            clearBtn = CTkButton(
+                master=self.on_going_tab,
+                command=x1,
+                text='Clear'
+            )
+            clearBtn.pack(fill=X, expand=0)
+        except Exception as x:
+            messagebox.showerror("Error", str(x))
+        """finally:
+            print(self.exists_tab.winfo_children())"""
+
+
+app = main()
+app.mainloop()
