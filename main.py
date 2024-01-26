328ef53f1e9f086d547cfb8d96be61be2699b379
Q3U3 <39864872+TheCNLto@users.noreply.github.com>
2024-01-26 11:37:06 +0100

    build v2

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from tkinter import BOTH, X, messagebox
 from customtkinter import CTk, CTkTabview, CTkLabel, CTkButton, CTkToplevel, CTkEntry, CTkScrollableFrame, END
 from playsound import playsound
 
-
+# alert window
 class alerty_alert(CTkToplevel):
     def __init__(self, info):
         super().__init__()
@@ -26,85 +26,95 @@ class alerty_alert(CTkToplevel):
             command=self.clearAlert
         )
         self.btn1.pack(fill=BOTH, expand=1)
-        playsound("alert.mp3")
+        self.update()
+        self.minsize(self.winfo_width(), self.winfo_height())
+        # maybe not working
+        self.after(ms=200, func=playsound("alert.mp3"))
+
 
     def clearAlert(self):
         self.destroy()
 
-
+# default main window
 class main(CTk):
     def __init__(self):
         super().__init__()
+        # stored alert as a list
         self.alerts: list = []
         self.title("Alerty")
+        # tabs defined
         self.tabs = CTkTabview(master=self)
+        # creation tab
         self.create_tab = self.tabs.add("Create")
-        # self.exists_tab = self.tabs.add("Exists")
-        self.on_going_tab = self.tabs.add("On going")
-        self.tabs.pack(fill=BOTH, expand=1)
         self.lb_create_1 = CTkLabel(
             master=self.create_tab,
             text="Title of the alert:"
         )
-        self.lb_create_1.pack(fill=BOTH, expand=1)
         self.alert_title = CTkEntry(
             master=self.create_tab,
             height=10,
             placeholder_text='Something...'
         )
-        self.alert_title.pack(fill=BOTH, expand=1)
         self.lb_create_about = CTkLabel(
             master=self.create_tab,
             text="Description of the alert:"
         )
-        self.lb_create_about.pack(fill=BOTH, expand=1)
         self.alert_about = CTkEntry(
             master=self.create_tab,
             height=10,
             placeholder_text='Some explanation...'
         )
-        self.alert_about.pack(fill=BOTH, expand=1)
-        self.lb_create_time = CTkLabel(
-            master=self.create_tab,
-            text="Time of the alert:"
-        )
-        self.lb_create_time.pack(fill=BOTH, expand=1)
         self.alert_time = CTkEntry(
             master=self.create_tab,
             height=10,
             placeholder_text='Like 14:32'
         )
-        self.alert_time.pack(fill=BOTH, expand=1)
+        self.lb_create_time = CTkLabel(
+            master=self.create_tab,
+            text="Time of the alert:"
+        )
         self.alert_create_button = CTkButton(
             text="Create timed alert",
             master=self.create_tab,
             command=self.createTimedAlert,
             corner_radius=50
         )
+        self.tabs.pack(fill=BOTH, expand=1)
+        self.lb_create_1.pack(fill=BOTH, expand=1)
+        self.alert_title.pack(fill=BOTH, expand=1)
+        self.lb_create_about.pack(fill=BOTH, expand=1)
+        self.alert_about.pack(fill=BOTH, expand=1)
+        self.lb_create_time.pack(fill=BOTH, expand=1)
+        self.alert_time.pack(fill=BOTH, expand=1)
         self.alert_create_button.pack(fill=BOTH, expand=1, pady=10)
+        # created timers
+        self.on_going_tab = self.tabs.add("On going")
         self.scrollableFrame = CTkScrollableFrame(
             master=self.on_going_tab,
         )
+        # scrollable frame for easy scroll
         self.scrollableFrame.pack(fill=BOTH, expand=1)
+        # update the class for correct sizes
+        self.update()
+        # prevent collapse
         self.minsize(self.winfo_width(), self.winfo_height())
+        # place the window the center of the screen
         self.eval('tk::PlaceWindow . center')
+        # time checker (loop starter)
         self.checker()
 
     def checker(self):
-        """current_time = time.localtime()
-        current_datetime = datetime.fromtimestamp(time.mktime(current_time))
-        new_datetime = current_datetime + timedelta(minutes=1)
-        new_time = time.localtime(time.mktime(new_datetime.timetuple()))"""
         for i in self.alerts:
             if i[2] <= time.strftime("%H:%M", time.localtime()):
                 alerty_alert(i)
                 print("time!!!!!")
                 self.alerts.remove(i)
+        # runs infinitely
         self.after(200, self.checker)
 
     def createTimedAlert(self):
-        # print(len(self.alert_title.get("0.0", "end")))
         try:
+            # needed basic information of the alert
             storeTitle, storeAbout, storeTime = self.alert_title.get(), self.alert_about.get(), self.alert_time.get()
             if len(storeTime) < 1 or len(storeTitle) < 1 or len(storeAbout) < 1:
                 messagebox.showerror("Error", "You forgot something important!")
@@ -115,11 +125,13 @@ class main(CTk):
                     text=f"{storeTitle}\n{storeAbout}\n{storeTime}"
                 )
                 lbCreated.pack(fill=X, expand=0, pady=20)
+                # after a successful creation clears itself
                 self.alert_time.delete(first_index=0, last_index=END)
                 self.alert_about.delete(first_index=0, last_index=END)
                 self.alert_title.delete(first_index=0, last_index=END)
                 self.alert_title.focus()
 
+            # remove button function for expired alerts
             def x1():
                 try:
                     self.alerts.remove(storeTime)
@@ -137,8 +149,6 @@ class main(CTk):
             clearBtn.pack(fill=X, expand=0)
         except Exception as x:
             messagebox.showerror("Error", str(x))
-        """finally:
-            print(self.exists_tab.winfo_children())"""
 
 
 app = main()
